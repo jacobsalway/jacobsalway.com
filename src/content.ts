a79aee89ea87f5c998a725c51b82c007fee87d91
Jacob Salway <jacob.salway@gmail.com>
2021-07-08 08:58:45 +1000

    Test changing contents

--- src/content.ts
+++ src/content.ts
@@ -2,7 +2,7 @@ export const hero = {
     'heroText': {
         'name': 'Jacob Salway',
         'intro': 'I work with all things data.',
-        'subtext': ["I'm a technical data analyst at <a href='https://domain.com.au' target='_blank'>Domain</a> and recent graduate from the <a href='https://www.sydney.edu.au/' target='_blank'>University of Sydney</a> with a Bachelor of Science majoring in computer science and data science. My current work is primarily data engineering along with a blend of data science, visualisation, and analytics."]
+        'subtext': ["I'm a Technical Data Analyst at <a href='https://domain.com.au' target='_blank'>Domain</a> and recent graduate from the <a href='https://www.sydney.edu.au/' target='_blank'>University of Sydney</a> with a Bachelor of Science majoring in computer science and data science. My current work is primarily data engineering along with a blend of data science, visualisation, and analytics."]
     }
 }
 
