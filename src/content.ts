660f8885dea1f9ac2fa03978f628986c71f4bbca
Jacob Salway <jacob.salway@gmail.com>
2021-07-31 15:13:40 +1000

    Update CI, content and hero SASS

--- src/content.ts
+++ src/content.ts
@@ -2,7 +2,7 @@ export const hero = {
     'heroText': {
         'name': 'Jacob Salway',
         'intro': 'I work with all things data.',
-        'subtext': ["I'm a technical data analyst at <a href='https://domain.com.au' target='_blank'>Domain</a> and recent graduate from the <a href='https://www.sydney.edu.au/' target='_blank'>University of Sydney</a> with a Bachelor of Science majoring in computer science and data science. My current work at Domain is primarily data engineering along with a blend of data science, visualisation, and analytics. In my spare time, I've been learning JavaScript, TypeScript, React, and Scala."]
+        'subtext': ["I'm a technical data analyst at <a href='https://domain.com.au' target='_blank'>Domain</a> and recent graduate from the <a href='https://www.sydney.edu.au/' target='_blank'>University of Sydney</a> with a Bachelor of Science majoring in computer science and data science. My current work at Domain is a combination of data engineering, analytics, and visualisation.", "I have experience using Python, R, Java, and SQL and working with Tableau, Spark, Snowflake, Airflow, and AWS services including EMR and SageMaker. In my spare time, I've been learning and working on projects with JavaScript, TypeScript, React, and Scala."]
     }
 }
 
