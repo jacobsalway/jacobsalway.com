0522b0c589ea8029aa19e17da994f62907db23b9
Jacob Salway <jacob.salway@gmail.com>
2021-10-04 19:34:16 +1100

    Add content

---
+++ content.ts
@@ -0,0 +1,23 @@
+export const hero = {
+    'heroText': {
+        'name': 'Jacob Salway',
+        'intro': 'I work with all things data.',
+        'subtext': ["I'm a technical data analyst at <a href='https://domain.com.au' target='_blank'>Domain</a> and recent graduate from the <a href='https://www.sydney.edu.au/' target='_blank'>University of Sydney</a> with a Bachelor of Science majoring in computer science and data science. My current work at Domain is a combination of data engineering, analytics, and visualisation.", "I have experience using Python, R, Java, and SQL and working with Tableau, Spark, Snowflake, Airflow, and AWS services including EMR and SageMaker. In my spare time, I've been learning and working on projects with JavaScript, TypeScript, React, and Scala."]
+    }
+}
+
+export const terminal = {
+    'output': ['jacob@computer:~$ python3 script.py']
+}
+
+export const posts = [
+    {
+        title: 'First blog post!',
+        id: 'first-blog-post',
+        time: new Date('2020-10-04'),
+        content: [
+            'Welcome! This is the first blog post on my website. In case you didn\'t read the homepage, I\'m a recent computer science and data science graduate from the University of Sydney and a data analyst/engineer at work. At work I do a combination of data analytics, engineering and visualisation, along with a bit of data science work if I get the chance.',
+            'I plan to post about to post about anything I come across during my work or side projects that I think would be useful to other data professionals. I hope you\'ll find what I post helpful.'
+        ]
+    }
+]
