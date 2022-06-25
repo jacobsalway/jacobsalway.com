c91640b1d8ca6425dc951ee3d7d7acb04748d6ea
Jacob Salway <jacob.salway@gmail.com>
2022-06-25 12:17:42 +1000

    Add blog posts to homepage (#5)
    * bold company name on tagline
    
    * update link styling
    
    * add recent posts to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,41 @@
 import Container from '@components/Container'
 import { getHero } from '@lib/content'
-import { Hero } from '@types'
+import { getPosts, sortPostsByDate } from '@lib/posts'
+import { formatDate } from '@lib/utils'
+import { Hero, Post } from '@types'
 import type { GetStaticProps, NextPage } from 'next'
+import Link from 'next/link'
 
-type Props = { hero: Hero }
+type Props = { hero: Hero; topPosts: Post[] }
 
-const Home: NextPage<Props> = ({ hero }) => {
+const Home: NextPage<Props> = ({ hero, topPosts }) => {
     const { name, tagline, subtext } = hero
 
     return (
         <Container showFooter={false}>
-            <h1 className="mb-2 text-5xl font-bold">{name}</h1>
+            <h1 className="mb-2 text-4xl font-bold sm:text-5xl">{name}</h1>
             <h2
-                className="text-xl"
+                className="text-lg sm:text-xl"
                 dangerouslySetInnerHTML={{ __html: tagline }}
             />
             <div className="mt-4">
                 {subtext.map((e, i) => (
                     <div key={i} dangerouslySetInnerHTML={{ __html: e }} />
+                ))}
+            </div>
+            <h3 className="mt-16 text-2xl font-semibold">Recent posts</h3>
+            <div className="mt-4 grid gap-3 sm:grid-cols-3">
+                {topPosts.map((p) => (
+                    <Link key={p.id} href={`/blog/${p.id}`}>
+                        <a className="flex flex-col justify-between rounded border p-4 no-underline shadow-md dark:border-gray-500">
+                            <div className="text-lg font-semibold">
+                                {p.title}
+                            </div>
+                            <div className="mt-6 text-sm text-gray-400">
+                                {formatDate(p.date)}
+                            </div>
+                        </a>
+                    </Link>
                 ))}
             </div>
         </Container>
@@ -26,9 +44,11 @@ const Home: NextPage<Props> = ({ hero }) => {
 
 export const getStaticProps: GetStaticProps<Props> = () => {
     const hero = getHero()
+    const posts = getPosts()
+    const topPosts = sortPostsByDate(posts).slice(0, 3)
 
     return {
-        props: { hero },
+        props: { hero, topPosts },
     }
 }
 
