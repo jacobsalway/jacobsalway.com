777a0c1d2e384a6b489ddd8b3f4bd86f87443e0d
Jacob Salway <jacob.salway@gmail.com>
2022-08-14 00:03:34 +1000

    add mock server

---
+++ mock/app.py
@@ -0,0 +1,15 @@
+import json
+import random
+import time
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api/post-views/<string:id>', methods=["GET"])
+def index(id: str):
+    time.sleep(1)
+    return json.dumps(random.randint(1, 10000))
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
