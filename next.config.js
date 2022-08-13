c39c0c28d6ce4b128d61308c185b0188fe719fbb
Jacob Salway <jacob.salway@gmail.com>
2022-08-13 23:01:32 +1000

    proxy local api calls

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  trailingSlash: true,
+    reactStrictMode: true,
+    trailingSlash: true,
+    rewrites: async () => {
+        // proxy local api requests to mock server
+        return [
+            {
+                source: '/api/:slug*',
+                destination: `http://localhost:5000/api/:slug*`,
+            },
+        ]
+    },
 }
