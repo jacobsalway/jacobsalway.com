6bbd355eeae2e4fce2e351cb30b7d842984b4801
Jacob Salway <jacob.salway@gmail.com>
2022-09-05 19:25:31 +1000

    feat: allow intraday compaction with existing file check and download

--- infra/lambdas/compact-cloudfront-logs/main.py
+++ infra/lambdas/compact-cloudfront-logs/main.py
@@ -5,6 +5,7 @@ import os
 import zoneinfo
 
 import boto3
+import botocore.exceptions
 
 BUCKET = os.environ["BUCKET"]
 
@@ -15,6 +16,17 @@ def list_raw_log_keys(s3) -> list[str]:
         Prefix="jacobsalway.com/raw/"
     )
     return [content["Key"] for content in response["Contents"]]
+
+
+def key_exists(key: str, s3) -> bool:
+    try:
+        s3.head_object(Bucket=BUCKET, Key=key)
+        return True
+    except botocore.exceptions.ClientError as e:
+        e.responser
+        if e.response['ResponseMetadata']['HTTPStatusCode'] == 404:
+            return False
+        raise e
 
 
 def get_current_date() -> str:
@@ -30,7 +42,12 @@ def lambda_handler(event, context):
     if not os.path.isdir("/tmp/logs/"):
         os.mkdir("/tmp/logs")
 
-    for key in list_raw_log_keys(s3):
+    raw_log_keys = list_raw_log_keys(s3)
+    if not raw_log_keys:
+        print("No logs to compact, so exiting early")
+        return {"message": "success"}
+
+    for key in raw_log_keys:
         download_path = key.replace("/", "_")
         s3.download_file(
             Bucket=BUCKET,
@@ -39,13 +56,23 @@ def lambda_handler(event, context):
         )
 
     current_date = get_current_date()
+    key = f"jacobsalway.com/processed/{current_date}.gz"
+    if key_exists(key, s3):
+        s3.download_file(
+            Bucket=BUCKET,
+            Key=key,
+            Filename=f"/tmp/{current_date}.gz"
+        )
+
     combined_path = f"/tmp/{current_date}.gz"
-    with gzip.open(combined_path, "wt") as f_out:
+    mode = "at" if os.path.exists(combined_path) else "wt"
+    with gzip.open(combined_path, mode) as f_out:
         for i, file in enumerate(glob.glob("/tmp/logs/*.gz")):
             with gzip.open(file, "rt") as f_in:
                 lines = f_in.readlines()
-                # skip header after the first file
-                if i > 0:
+                # skip header if appending to existing logs
+                # or if not first file
+                if i > 0 or mode == "at":
                     lines = lines[2:]
                 f_out.writelines(lines)
 
