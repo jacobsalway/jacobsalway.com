1886d97718d10cc665e2c840510a62c2cd1fdc48
Jacob Salway <jacob.salway@gmail.com>
2022-10-14 22:49:12 +1100

    feat: align dates to right in post list

--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -6,12 +6,14 @@ import React from 'react'
 const PostView: React.FC<Post> = ({ title, id, date }) => {
     return (
         <div key={id}>
-            <h1 className="text-lg md:text-xl">
-                <Link href={`/blog/${id}`}>{title}</Link>
-                <span className="ml-3 whitespace-nowrap text-sm text-gray-400">
-                    {formatDate(date, false)}
-                </span>
-            </h1>
+            <Link href={`/blog/${id}`}>
+                <a className="group flex items-baseline justify-between text-lg no-underline md:text-xl">
+                    <h1 className="underline">{title}</h1>
+                    <span className="ml-3 whitespace-nowrap text-sm text-gray-400 group-hover:text-blue-500">
+                        {formatDate(date, false)}
+                    </span>
+                </a>
+            </Link>
         </div>
     )
 }
