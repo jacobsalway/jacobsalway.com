8eed556079559cc3e3475dfba65c3cfbc98e18d9
Jacob Salway <jacob.salway@gmail.com>
2023-01-02 20:50:58 +1100

    feat: explicitly set line number style

--- src/components/markdown/CodeFormatter.tsx
+++ src/components/markdown/CodeFormatter.tsx
@@ -1,8 +1,20 @@
 import styles from '@styles/Blog.module.sass'
+import type { CSSProperties } from 'react'
 import { CodeComponent } from 'react-markdown/lib/ast-to-react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import CopyButton from '../CopyButton'
+
+// set a fixed width for the line numbers
+// so that all code in the post is aligned
+const lineNumberStyle = {
+    minWidth: '1.5em',
+    paddingRight: '1em',
+    fontWeight: 'normal',
+    textAlign: 'left',
+    userSelect: 'none',
+    color: '#888',
+} as CSSProperties
 
 const CodeFormatter: CodeComponent = ({
     inline,
@@ -16,13 +28,11 @@ const CodeFormatter: CodeComponent = ({
     return !inline && match ? (
         <div className={styles.codeBlock}>
             <SyntaxHighlighter
-                style={darcula}
+                style={vscDarkPlus}
                 language={match[1]}
                 showLineNumbers={true}
-                lineNumberStyle={{
-                    textAlign: 'left',
-                    minWidth: '2.5em',
-                }}
+                showInlineLineNumbers={false}
+                lineNumberStyle={lineNumberStyle}
                 customStyle={{ padding: 0, margin: 0, background: 'none' }}
             >
                 {code}
