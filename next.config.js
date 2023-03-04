dc91709085c9fbfbcb5d832498a09cee77f53a5f
Jacob Salway <jacob.salway@gmail.com>
2023-03-04 14:38:20 +1100

    chore: remove AWS related code

--- next.config.js
+++ next.config.js
@@ -2,13 +2,4 @@
 module.exports = {
     reactStrictMode: true,
     trailingSlash: true,
-    rewrites: async () => {
-        // proxy local api requests to mock server
-        return [
-            {
-                source: '/api/:slug*',
-                destination: `http://localhost:5000/api/:slug*`,
-            },
-        ]
-    },
 }
