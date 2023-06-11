87872ca24cfc984b9e716478fb33ca399b99af52
Jacob Salway <jacob.salway@gmail.com>
2023-06-11 17:35:23 +1000

    feat: add prefetch and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://www.jacobsalway.com",
+  integrations: [tailwind(), sitemap(), prefetch()],
 });
