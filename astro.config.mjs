7311e9c8db4ff91748cb9e12e2bb2f58aa15cc78
Jacob Salway <jacob.salway@gmail.com>
2024-04-06 20:12:13 +1100

    Upgrade dependencies and minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.jacobsalway.com",
-  integrations: [tailwind(), sitemap(), prefetch()],
+  integrations: [tailwind(), sitemap()],
+  prefetch: true,
 });
