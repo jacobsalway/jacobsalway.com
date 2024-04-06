03037ba416d79cc937b8a47bdbcd4187d0ff65e4
Jacob Salway <jacob.salway@gmail.com>
2024-04-06 20:23:08 +1100

    Fix with Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://www.jacobsalway.com",
   integrations: [tailwind(), sitemap()],
   prefetch: true,
+  output: "static",
+  adapter: vercel(),
 });
