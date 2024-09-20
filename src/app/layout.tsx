4c1034ffc4719d91689572fa0e628b04de8f80f7
Jacob Salway <jacob.salway@gmail.com>
2024-09-20 11:11:31 +1000

    Add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { Metadata } from "next";
 import Link from "next/link";
 import "./globals.css";
@@ -33,6 +34,7 @@ export default function RootLayout({
           </nav>
         </header>
         {children}
+        <Analytics />
       </body>
     </html>
   );
