9f6e972a40cc62a9ba8f519e7d6f05b9163cf539
Jacob Salway <jacob.salway@gmail.com>
2026-08-15 11:53:50 -0700

    Upgrade dependencies and migrate to ESLint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier'
+
+const config = [...nextVitals, ...nextTs, prettier]
+
+export default config
